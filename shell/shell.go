8730cc06d259ca96f233ba245ae94dff7846108b
Martin Bernstorff <martinbernstorff@gmail.com>
2024-08-15 18:53:45 +0200

    update shell.go

--- shell/shell.go
+++ shell/shell.go
@@ -9,7 +9,7 @@ func Run(
 	command string,
 ) error {
 	// Figure out the calling shell
-	cmd := exec.Command(os.Getenv("SHELL") + "-c" + command)
+	cmd := exec.Command(os.Getenv("SHELL") + " -c " + command)
 
 	// Set up pipes for standard input, output, and error
 	cmd.Stdin = os.Stdin
