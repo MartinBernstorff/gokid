84dd20f8ec248eac453407a4cb30a522ba3720b0
Martin Bernstorff <martinbernstorff@gmail.com>
2024-08-15 18:58:37 +0200

    core functionality (#1)
    * core_functionality
    
    * update shell.go
    
    * update new.go and shell.go
    
    * update shell.go
    
    * update shell.go
    
    * update shell.go
    
    * update shell.go
    
    * update ready.go

--- shell/shell.go
+++ shell/shell.go
@@ -7,14 +7,10 @@ import (
 
 func Run(
 	command string,
-	args ...string,
 ) error {
-	print("\nRunning ", command, args)
+	// Figure out the calling shell
 	shell := os.Getenv("SHELL")
-	all_args := append([]string{"-c", command}, args...)
-	cmd := exec.Command(shell, all_args...)
-
-	// Migrate the path to the new command
+	cmd := exec.Command(shell, []string{"-c", command}...)
 
 	// Set up pipes for standard input, output, and error
 	cmd.Stdin = os.Stdin
@@ -24,10 +20,7 @@ func Run(
 	// Execute the command
 	err := cmd.Run()
 	if err != nil {
-		print("\nCommand failed with error:", err)
 		panic(err)
 	}
-
-	print("\nCommand finished successfully\n\n")
 	return nil
 }
