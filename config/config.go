f4b2cce8f489e92e828066f92ead55005deb16b6
Martin Bernstorff <martinbernstorff@gmail.com>
2024-10-16 13:31:11 +0200

    update 5 files

---
+++ config/config.go
@@ -0,0 +1,60 @@
+package config
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/spf13/viper"
+)
+
+type GokidConfig struct {
+	Trunk string
+}
+
+func Init() GokidConfig {
+	configFile := findConfig(".gokid")
+	if configFile != "" {
+		viper.SetConfigFile(configFile)
+		fmt.Println("Using config file:", configFile)
+	} else {
+		fmt.Println("No config file found")
+	}
+
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Println("Error reading config file:", err)
+	}
+
+	return GokidConfig{
+		Trunk: viper.GetString("trunk"),
+	}
+}
+
+func findConfig(configName string) string {
+	configExtensions := []string{".yaml", ".yml", ".json", ".toml"}
+
+	dir, err := os.Getwd()
+	if err != nil {
+		fmt.Println("Error getting current directory:", err)
+		return ""
+	}
+
+	for {
+		for _, ext := range configExtensions {
+			configPath := filepath.Join(dir, configName+ext)
+			if _, err := os.Stat(configPath); err == nil {
+				return configPath
+			}
+		}
+
+		// Move to the parent directory
+		parent := filepath.Dir(dir)
+		if parent == dir {
+			// We've reached the root directory
+			break
+		}
+		dir = parent
+	}
+
+	return ""
+}
