bbaa408919461b13cba5298a06b28941fc8f14cd
Martin Bernstorff <martinbernstorff@gmail.com>
2024-10-16 13:52:28 +0200

    update .gokid.yml, ready.go and config.go

---
+++ cmd/merge.go
@@ -0,0 +1,47 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"gokid/config"
+	"gokid/shell"
+	"slices"
+
+	"github.com/spf13/cobra"
+)
+
+func merge() {
+	cfg := config.Init()
+
+	cmd := "gh pr merge"
+
+	allowedStrategies := []string{"squash", "rebase", "merge"}
+
+	if !slices.Contains(allowedStrategies, cfg.MergeStrategy) {
+		fmt.Printf("Merge strategy is not allowed, allowed are: %s", allowedStrategies)
+	} else {
+		cmd += fmt.Sprintf(" --%s", cfg.MergeStrategy)
+	}
+
+	if cfg.AutoMerge {
+		cmd += " --auto"
+	}
+
+	shell.Run(cmd)
+}
+
+var mergeCmd = &cobra.Command{
+	Use:   "merge",
+	Short: "Merge a change",
+	Long:  "",
+	Run: func(cmd *cobra.Command, args []string) {
+		merge()
+	},
+	Aliases: []string{"m"},
+}
+
+func init() {
+	rootCmd.AddCommand(mergeCmd)
+}
