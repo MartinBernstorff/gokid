8006fca2fbcb968a82de68b173add3cb6fddacc5
Martin Bernstorff <martinbernstorff@gmail.com>
2024-10-16 14:20:24 +0200

    update issue.go

---
+++ cmd/view.go
@@ -0,0 +1,23 @@
+package cmd
+
+import (
+	"gokid/forge"
+
+	"github.com/spf13/cobra"
+)
+
+func view() {
+	forge.View()
+}
+
+func init() {
+	rootCmd.AddCommand(&cobra.Command{
+		Use:   "View",
+		Short: "View the change at the forge",
+		Long:  "",
+		Run: func(cmd *cobra.Command, args []string) {
+			view()
+		},
+		Aliases: []string{"v"},
+	})
+}
