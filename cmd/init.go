7aa0e655e132371e09bbf7c44b214313bdaae05f
Martin Bernstorff <martinbernstorff@gmail.com>
2024-10-29 14:23:43 +0100

    update cfg.go

---
+++ cmd/init.go
@@ -0,0 +1,77 @@
+package cmd
+
+import (
+	"fmt"
+	"gokid/config"
+	"io"
+	"os"
+	"strings"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+func updateGitignore() error {
+	gitignorePath := ".gitignore"
+	ignoreString := ".gokid.*"
+
+	// Create .gitignore if it doesn't exist
+	if _, err := os.Stat(gitignorePath); os.IsNotExist(err) {
+		file, err := os.Create(gitignorePath)
+		if err != nil {
+			return fmt.Errorf("error creating .gitignore file: %w", err)
+		}
+		defer file.Close()
+		_, err = file.WriteString(ignoreString + "\n")
+		return err
+	}
+
+	// Update existing .gitignore
+	file, err := os.OpenFile(gitignorePath, os.O_RDWR, 0644)
+	if err != nil {
+		return fmt.Errorf("error opening .gitignore file: %w", err)
+	}
+	defer file.Close()
+
+	contents, err := io.ReadAll(file)
+	if err != nil {
+		return fmt.Errorf("error reading .gitignore file: %w", err)
+	}
+
+	if !strings.Contains(string(contents), ignoreString) {
+		if !strings.HasSuffix(string(contents), "\n") {
+			ignoreString = "\n" + ignoreString
+		}
+		_, err = file.WriteString(ignoreString + "\n")
+		if err != nil {
+			return fmt.Errorf("error writing to .gitignore file: %w", err)
+		}
+	}
+
+	return nil
+}
+
+var initCmd = &cobra.Command{
+	Use:   "init",
+	Short: "Initialize gokid configuration",
+	Long:  "Creates a default configuration file and updates .gitignore",
+	Run: func(cmd *cobra.Command, args []string) {
+		config.Init() // Load or create default config
+
+		if err := viper.SafeWriteConfigAs(".gokid.yml"); err != nil {
+			fmt.Fprintf(os.Stderr, "Error writing config file: %v\n", err)
+			os.Exit(1)
+		}
+
+		if err := updateGitignore(); err != nil {
+			fmt.Fprintf(os.Stderr, "Error updating .gitignore: %v\n", err)
+			os.Exit(1)
+		}
+
+		fmt.Println("Successfully initialized gokid configuration")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(initCmd)
+}
