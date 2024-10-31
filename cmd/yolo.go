42437407164cc35e620d4f473af5e116430e3e7c
Martin Bernstorff <martinbernstorff@gmail.com>
2024-10-31 10:26:55 +0100

    update yolo.go and config.go

--- cmd/yolo.go
+++ cmd/yolo.go
@@ -23,12 +23,12 @@ func NewYoloer(merger *Merger) *Yoloer {
 	}
 }
 
-func (y *Yoloer) yolo(draft bool, mergeStrategy string, confirmed bool) {
+func (y *Yoloer) yolo(draft bool, mergeStrategy string, confirmed bool, preYoloCommand string) {
 	if !confirmed {
 		fmt.Println("Aborted.")
 		return
 	}
-	y.merger.merge("", false, true, draft, mergeStrategy, "", false)
+	y.merger.merge(preYoloCommand, false, true, draft, mergeStrategy, "", false)
 }
 
 func init() {
