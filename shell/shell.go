f7b082c9675654fcd4e840fe83861247836b17cd
Martin Bernstorff <martinbernstorff@gmail.com>
2025-01-30 20:14:42 +0100

    update config.go and shell.go

--- shell/shell.go
+++ shell/shell.go
@@ -8,6 +8,7 @@ import (
 
 type Shell interface {
 	Run(cmd string) error
+	RunWithOutput(cmd string) (string, error)
 }
 
 type RealShell struct{}
@@ -33,4 +34,15 @@ func (s *RealShell) Run(cmd string) error {
 		panic(err)
 	}
 	return nil
+}
+func (s *RealShell) RunWithOutput(cmd string) (string, error) {
+	shell := os.Getenv("SHELL")
+	command := exec.Command(shell, "-c", cmd)
+
+	// Set up pipes for standard output and error
+	output, err := command.Output()
+	if err != nil {
+		return "", err
+	}
+	return string(output), nil
 }
